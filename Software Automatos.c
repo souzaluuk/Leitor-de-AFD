155c9fc6479cda0d8e36809d699d5a93402b0400
Lucas Souza <lucas.souza.gabriel@outlook.com>
2017-08-30 21:11:37 -0300

    Inserção de arquivos do software

---
+++ Software Automatos.c
@@ -0,0 +1,112 @@
+#include "Software Automatos.h"
+
+int main()
+{
+	FILE *fp;
+	CADEIA *alf = NULL, *palavra = NULL;
+	ESTADO *est = NULL;
+	ESTADO *est_finais = NULL;
+	REGRA *regra = NULL;
+	
+	char letra = ' ', estado_inicio[4], estado_fim[4], caminho_arq[] = "automato.txt";
+	int cont = 0, cont_virg = 0;
+	
+	fp = fopen(caminho_arq, "r");
+	if ( fp == NULL ){
+		printf("Arquivo '%s' nao encontrado\n",caminho_arq);
+		system("pause");
+		return 0;
+	}
+	while ( letra != ')' ){
+		fscanf(fp, "%c", &letra);
+		
+		if ( letra == ',' )
+			cont_virg++;
+			
+		if ( cont_virg == 3 ){
+			fscanf(fp, "%s", estado_inicio);
+			estado_inicio[strlen(estado_inicio)-1] = '\0'; //elimina virgula do fim da String
+			
+			if ( !valida_estado(est,estado_inicio) ){
+				printf("Estado Inicial Invalido!");
+				return 0;
+			}
+			cont_virg++;
+		}
+		//Conta o símbolo "{" para saber o ponto de "ignição" para a leitura de um novo conjunto
+		if (letra == '{'){
+			cont++;
+			
+			switch(cont){
+				case 1:
+					while ( letra != '}' ){
+						fscanf(fp, "%c", &letra);
+						if ( letra >= 'a' && letra <= 'z' || letra >= '0' && letra <= '9' ){
+							// Se o retorno for 1 significa que já existe um elemento
+							// de mesma letra na estrutura, logo, a letra atual é repetida.
+							// Assim o main informa que a letra é repetida e encerra o main,
+							//caso contrário, enfileira a letra na estrutura "alf"
+							if ( char_existe(alf,letra) ){
+								printf("Elemento \'%c' Repetido", letra);
+								return 0;
+							}
+							guarda_char(&alf,letra);
+						}
+					}
+					break;
+				case 2:
+					filtra_estados(&fp,&est);
+					if ( !valida_conjunto_est(est,est) ){
+						return 0;
+					}
+					break;
+				case 3:
+					filtra_estados(&fp,&est_finais);
+					if ( !valida_conjunto_est(est,est_finais) || !valida_conjunto_est(est_finais,est_finais)){
+						return 0;
+					}
+					break;
+				default:
+					printf("ERRO!");
+			}
+		}
+	}
+	
+	char est_origem[4], elemento[3], est_destino[4];
+	while( fscanf(fp, "%s %s %s", est_origem, elemento, est_destino) != EOF ){
+		//elimina a ',' (vírgula) da string
+		est_origem[strlen(est_origem)-1] = '\0';
+		elemento[strlen(elemento)-1] = '\0';
+		
+		if ( char_existe(alf,elemento[0]) && valida_estado(est,est_origem) && valida_estado(est,est_destino) ) {
+			enfileira_regra(&regra, est_origem, elemento[0], est_destino);
+		}else{
+			printf("Regra \'%s, %s, %s' invalida!\n",est_origem, elemento, est_destino);
+			printf("\nO estado a letra ou a sintaxe da regra podem estar incorreta!");
+			printf("\nReescreva como no exemplo:\n e.g.: \"qx, a, qy\"");
+			return 0;
+		}
+	}
+	
+	printf("Alfabeto: ");
+	print_char(alf);
+	printf("\nEstados: ");
+	imprime_est(est);
+	printf("\nEstado Inicial: %s", estado_inicio);
+	printf("\nEstados Finais: ");
+	imprime_est(est_finais);
+	printf("\nRegras: \n");
+	imprime_regra(regra);
+	fclose(fp);
+	
+	printf("\nInforme a palavra: ");
+	ler_palavra(&palavra);
+	
+	if ( palavra_valida(palavra,alf) )
+		ler_automato(palavra,regra,est_finais,estado_inicio);
+	else
+		printf("PALAVRA NAO ACEITA");
+	printf("\n");
+	system("pause");
+	return 0;
+}
---
+++ Software Automatos.h
@@ -0,0 +1,276 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+//Estrutura para Alfabeto e Palavra
+typedef struct conjunto_char{
+	char letra;
+	struct conjunto_char *prox;
+}CADEIA;
+
+//Estrutura para Regras
+typedef struct regras{
+	char estado_origem[4];
+	char estado_destino[4];
+	char letra;
+	struct regras *prox;
+}REGRA;
+
+//Estrutura para Estados
+typedef struct estados{
+	char estado[4];
+	struct estados *prox;
+}ESTADO;
+
+void guarda_char(CADEIA **p, char letra);
+void print_char(CADEIA *p);
+int vazio_char(CADEIA *p);
+int char_existe(CADEIA *p, char letra); //Se a letra exista retorna 1, se não 0
+void ler_palavra(CADEIA **p); //Usado para leitura de uma palavra do buffer na cadeia
+int tam_palavra(CADEIA *p); //Retorna o tamanho da cadeia
+int palavra_valida(CADEIA *p, CADEIA *alf); //Retorna 1 se a palavra contém algum caracter inexistente ao Alfabeto definido
+void imprime_passo(CADEIA *p, char estado[], int posi); //Imprime a transição atual
+void ler_automato(CADEIA *palavra, REGRA *regra, ESTADO *estados_finais, char estado_inicio[]); //
+char* prox_estado(REGRA *p, char estado_origem[], char letra); 
+
+int vazio_regra(REGRA *p);
+void imprime_regra(REGRA *p);
+void enfileira_regra(REGRA **p, char est_origem[], char letra, char est_destino[]);
+
+void enfileira_est(ESTADO **p, char estado[]);
+void imprime_est(ESTADO *p);
+int vazio_est(ESTADO *p);
+int valida_estado(ESTADO *est, char estado[]);
+int valida_conjunto_est(ESTADO *est_princ, ESTADO *est_secun);
+void filtra_estados(FILE **fp, ESTADO **p);
+
+void ler_automato(CADEIA *palavra, REGRA *regra, ESTADO *estados_finais, char estado_inicio[]){
+	CADEIA *tmp = palavra;
+	
+	int tam = tam_palavra(palavra), i;
+	//char palavra_tmp[tam];
+	char est_tmp[4];
+	
+	printf("Palavra: ");
+	print_char(palavra);
+	printf("\n");
+
+	for ( i=0; i<tam; i++ ){		
+		if ( prox_estado(regra,estado_inicio,palavra->letra) != NULL ){
+			strcpy(est_tmp,estado_inicio);
+			strcpy(estado_inicio,prox_estado(regra,estado_inicio,palavra->letra));
+			
+			palavra = palavra->prox;
+		}else{
+			imprime_passo(tmp,estado_inicio,i);
+			printf("\nPALAVRA NAO ACEITA");
+			return;
+		}
+		imprime_passo(tmp,est_tmp,i);
+	}
+	
+	imprime_passo(tmp,estado_inicio,i);
+	if ( valida_estado(estados_finais,estado_inicio) )
+		printf("\nPALAVRA ACEITA");
+	else
+		printf("\nPALAVRA NAO ACEITA");
+}
+
+char* prox_estado(REGRA *p, char estado_origem[], char letra){
+	while( !vazio_regra(p) ){
+		if ( strcmp(estado_origem,p->estado_origem) == 0 && p->letra == letra)
+			return p->estado_destino;
+		p = p->prox;
+	}	
+	return NULL;
+}
+
+void imprime_passo(CADEIA *p, char estado[], int posi){
+	int i = 0;
+	
+	while( !vazio_char(p) ){
+		//printf("(%s,%c) -> %s\n", est_tmp,palavra->letra,estado_inicio);
+		if ( posi != i )
+			printf("%c",p->letra);
+		else
+			printf("{%s,%c}",estado,p->letra);
+		i++;
+		p = p->prox;
+	}
+	if ( posi == i )
+		printf("{%s}",estado);	
+	printf("\n");
+}
+
+int palavra_valida(CADEIA *p, CADEIA *alf){
+	while ( !vazio_char(p) ){
+		if ( !char_existe(alf,p->letra) ){
+			return 0;
+		}
+		p = p->prox;
+	}
+	return 1;
+}
+
+int tam_palavra(CADEIA *p){
+	int cont=0;
+	while(!vazio_char(p)){
+		cont++;
+		p = p->prox;
+	}
+	return cont;
+}
+
+void ler_palavra(CADEIA **p){
+	char letra = ' ';
+	
+	while (letra != '\n'){
+		letra = getchar();
+		if (letra != '\n')
+			guarda_char(&*p, letra);
+	}
+}
+
+void guarda_char(CADEIA **p, char letra){
+	CADEIA *p1 = (CADEIA*) malloc(sizeof(CADEIA)), *p2;
+	p1->letra = letra;
+	p1->prox = NULL;
+	if ( vazio_char(*p) ){
+		*p = p1;
+	}else{
+		p2 = *p;
+		while (p2->prox != NULL)
+			p2 = p2->prox;
+		p2->prox = p1;
+	}
+}
+void print_char(CADEIA *p){
+	while( !vazio_char(p) ){
+		printf("%c ",p->letra);
+		p = p->prox;
+	}
+}
+int vazio_char(CADEIA *p){
+	return(p == NULL)?1:0;
+}
+
+int char_existe(CADEIA *p, char letra){
+	int cont = 0;
+	//O laço percorre toda a fila do tipo ALFABETO
+	//verificando a existência da letra passada pra função atual
+	//no Conjunto Alfabeto
+	while( !vazio_char(p) ){
+		if ( p->letra == letra )
+			cont++;
+		p = p->prox;
+	}
+	//Caso exista apenas um correspondente é retornado 1
+	return ( cont == 1 )?1:0;
+}
+
+//Funções Estados
+void enfileira_est(ESTADO **p, char estado[]){
+	ESTADO *p1 = (ESTADO*)malloc(sizeof(ESTADO)), *p2;
+	//p1->estado = estado;
+	strcpy(p1->estado, estado);
+	p1->prox = NULL;
+	
+	if ( vazio_est(*p) ){
+		*p = p1;
+	}else{
+		p2 = *p;
+		while (p2->prox != NULL)
+			p2 = p2->prox;
+		p2->prox = p1;
+	}
+}
+int vazio_est(ESTADO *p){
+	return(p == NULL)?1:0;
+}
+
+void imprime_est(ESTADO *p){
+	while( !vazio_est(p) ){
+		printf("%s ",p->estado);
+		p = p->prox;
+	}
+}
+
+int tam_est(char estado[]){
+	int i = 0;
+	while(estado[i] != '\0')
+		i++;
+		
+	return i;
+}
+
+void filtra_estados(FILE **fp, ESTADO **p){
+	char letra = ' ';
+	while ( letra != '}' ){
+		fscanf(*fp, "%c", &letra);
+		if ( letra == 'q' ){
+			int cont_est = 0;
+			char estado_1[4] = {};		
+			estado_1[cont_est] = letra;
+			
+			while ( letra != ',' && letra != '}' ){
+				fscanf(*fp, "%c", &letra);
+				if ( letra >= '0' && letra <= '9' || letra == 'f' ){
+					cont_est++;
+					estado_1[cont_est] = letra;
+				}
+			}
+			if ( tam_est(estado_1) > 1 )
+				enfileira_est(&*p,estado_1);
+		}
+	}
+}
+
+int valida_estado(ESTADO *est, char estado[]){
+	int cont = 0;
+	
+	while ( !vazio_est(est) ){
+		if ( strcmp(est->estado, estado) == 0 )
+			cont++;
+		est = est->prox;
+	}
+	return (cont == 1)?1:0;
+}
+
+int valida_conjunto_est(ESTADO *est_princ, ESTADO *est_secun){
+	while ( !vazio_est(est_secun) ){
+		if ( !valida_estado(est_princ,est_secun->estado) ){
+			printf("Estado \'%s' Invalido", est_secun->estado);
+			return 0;
+		}
+		est_secun = est_secun->prox;
+	}
+	return 1;
+}
+
+//Funções Regras
+void enfileira_regra(REGRA **p, char est_origem[], char letra, char est_destino[]){
+	REGRA *p1 = (REGRA*)malloc(sizeof(REGRA)), *p2;
+	
+	strcpy(p1->estado_origem, est_origem);
+	strcpy(p1->estado_destino, est_destino);
+	p1->letra = letra;
+	p1->prox = NULL;
+	
+	if ( vazio_regra(*p) ){
+		*p = p1;
+	}else{
+		p2 = *p;
+		while (p2->prox != NULL)
+			p2 = p2->prox;
+		p2->prox = p1;
+	}
+}
+int vazio_regra(REGRA *p){
+	return(p == NULL)?1:0;
+}
+void imprime_regra(REGRA *p){
+	while( !vazio_regra(p) ){
+		printf("%s, %c, %s\n",p->estado_origem, p->letra, p->estado_destino);
+		p = p->prox;
+	}
+}
